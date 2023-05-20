54859f09b2b4c57888da6f120729b6d6094c7066
Sandro Bretterklieber <sandro.bretterklieber@rebug.org>
2023-05-20 11:50:05 +0200

    did error handling for fields without min/max items

--- cantools/database/diagnostics/did.py
+++ cantools/database/diagnostics/did.py
@@ -180,7 +180,7 @@ class Did(object):
 
         for sub_data_obj in self.datas:
             if sub_data_obj.sub_elements:
-                if sub_data_obj.qty =='field':
+                if sub_data_obj.qty == 'field' and sub_data_obj.max_num_of_items and sub_data_obj.maximum:
                     sub_data_obj._codec = {
                         'datas': sub_data_obj.sub_elements,
                         'formats': create_encode_decode_formats(sub_data_obj.sub_elements,
